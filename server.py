a9d4bbdb4a55b8237ab7e195b1e4483d92628f00
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-06-23 12:16:12 +0200

    setup on startup and shutdown for service

--- server.py
+++ server.py
@@ -6,3 +6,18 @@ app = FastAPI()
 def root():
     return {"message": "Hello from backoffice"}
 
+def callback(ch, method, properties, body):
+    pass
+
+def main():
+    print("\\ main function called \\")
+
+@app.on_event("startup")
+def on_startup():
+    # create helper object to handle messages from rabbitmq
+    main()
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    # shutdown rabbitmq
+    pass
